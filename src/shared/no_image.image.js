d9bc959b249f3f6dd31596d45dd183393f53087b
ShubhamMore <shubhammore1796@gmail.com>
2019-10-07 13:07:29 +0530

    Dancing Soul Backend

--- src/shared/no_image.image.js
+++ src/shared/no_image.image.js
@@ -2,7 +2,9 @@ const image = {
     "created_at": "2019-09-29T09:48:56Z",
     "image_name": "1569750534235.png",
     "secure_url": "https://res.cloudinary.com/shubhamore/image/upload/v1569750536/shared/1569750534235.png",
-    "public_id": "shared/1569750534235"
+    "public_id": "shared/1569750534235",
+    "width": "400",
+    "height": "300"
 }
 
 module.exports = image
