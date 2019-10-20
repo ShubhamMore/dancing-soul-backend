4c25da912f74877983d0f78985ddf64e91fa9c82
ShubhamMore <shubhammore1796@gmail.com>
2019-10-20 13:53:13 +0530

    Dancing Soul Backend

--- src/shared/no_image.image.js
+++ src/shared/no_image.image.js
@@ -2,7 +2,7 @@ const image = {
     created_at: "2019-10-11T16:54:42Z",
     image_name: "shared0-1570812880853.png",
     public_id: "shared/shared0-1570812880853",
-    secure_url: "https://res.cloudinary.com/shubhamore/image/upload/v1570812882/shared/,shared0-1570812880853.png",
+    secure_url: "https://res.cloudinary.com/shubhamore/image/upload/v1570812882/shared/shared0-1570812880853.png",
     height: "150",
     width: "200"
 }
